87e9d68d331f3fd9cbe14b9ccedc5ab7f4df80be
gsaikrishna515 <79789355+gsaikrishna515@users.noreply.github.com>
2025-09-23 15:44:34 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -69,7 +69,7 @@ resource "aws_eks_node_group" "node_group" {
     aws_subnet.private_b.id
   ]
 
-  instance_types = ["t3.large"]
+  instance_types = ["t3.medium"]
   scaling_config {
     desired_size = 2
     max_size     = 3
